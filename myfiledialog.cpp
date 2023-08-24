a5f9a003b00e2fef94bf520fc656297e69dce149
Marator2K207 <dmitriyshaaa@gmail.com>
2023-08-24 12:30:14 +0300

    Теперь класс MyFileDialog имеет возможность открывать файл и загружать его сдержимое в свой атрибут, переданный через конструктор.

--- myfiledialog.cpp
+++ myfiledialog.cpp
@@ -1,9 +1,25 @@
 
 #include "myfiledialog.h"
 
-MyFileDialog::MyFileDialog(QWidget *parent)
+MyFileDialog::MyFileDialog(QTextEdit *textField,
+                           QWidget *parent)
     : QFileDialog(parent)
+    , textField{textField}
 {
+}
 
+void MyFileDialog::loadTxtFileInField()
+{
+    // определяем местную функцию
+    auto fileContentReady = [&](const QString &fileName,
+                                const QByteArray &fileContent) {
+        if (fileName.isEmpty()) {
+            textField->setText("File no found or selected");
+        } else {
+            textField->setText(fileContent);
+        }
+    };
+    // используем функцию выше для открытия текстовых файлов
+    getOpenFileContent("Text files (*.txt)", fileContentReady);
 }
 
--- myfiledialog.h
+++ myfiledialog.h
@@ -4,12 +4,25 @@
 
 #include <QFileDialog>
 #include <QWidget>
+#include <QTextEdit>
+#include <QByteArray>
 
 class MyFileDialog : public QFileDialog
 {
     Q_OBJECT
+
+private:
+    QTextEdit *textField;
+
 public:
-    MyFileDialog(QWidget *parent = 0);
+    MyFileDialog(QTextEdit *textField,
+                 QWidget *parent = 0);
+
+public slots:
+    ///
+    /// \brief loadTxtFileInField
+    /// слот-метод загрузки данных текстового файла в поле
+    void loadTxtFileInField();
 };
 
 #endif // MYFILEDIALOG_H
