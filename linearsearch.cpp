3fdc2aaa37f842e6f4ba2166b2f76f49e60db166
Marator2K207 <dmitriyshaaa@gmail.com>
2023-08-24 10:34:43 +0300

    Доработан алгоритм линейного поиска.

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -11,10 +11,18 @@ LinearSearch::LinearSearch(QTextEdit *text,
 
 void LinearSearch::start()
 {
+
     QString txt = text->toPlainText();
     QString smpl = sample->text();
     int i = 0;
     int j = 0;
+
+    // сначала стоит проверить, а заполнены ли поля
+    if(txt.length() == 0 || smpl.length() == 0) {
+        emit resultIsReady(QString("Недостаточно информации"));
+        emit stop();
+        return;
+    }
 
     while (i < txt.length()) {
         if(txt[i] == smpl[j]) {
@@ -23,11 +31,13 @@ void LinearSearch::start()
         }
         else {
             j = 0;
+            i++;
         }
         // в случае нахождения образа в тексте
-        if(j == smpl.length() - 1) {
+        if(j == smpl.length()) {
             emit resultIsReady(QString("Образ найден на позиции: %1").arg(i-j));
             emit stop();
+            return;
         }
     }
     // если ничего не нашли
