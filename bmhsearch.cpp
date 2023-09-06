c8a2ad60c82b801424945535d8a8d8f0d7ac9edc
Marator2K07 <dmitriyshaaa@gmail.com>
2023-09-06 11:42:51 +0200

    Начало написания алгоритма поиска в публичном слоте start()

--- bmhsearch.cpp
+++ bmhsearch.cpp
@@ -6,10 +6,24 @@ BMPSearch::BMPSearch(QTextEdit* text,
 {
     this->text = text;
     this->sample = sample;
+    shift = new int[arrayShiftSize];
 }
 
 void BMPSearch::start()
 {
+    QString txt = text->toPlainText();
+    QString smpl = sample->text();
 
+    // стоит проверить, а заполнены ли поля
+    if(txt.length() == 0 || smpl.length() == 0) {
+        emit timeIsReady(QString::number(0));
+        emit resultIsReady(QString("Недостаточно информации"));
+        emit stop();
+        return;
+    }
+    // для начала проинициализируем массив сдвигов
+    for(int i = 0; i < arrayShiftSize; i++) {
+        shift[i] = txt.length();
+    }
 }
 
